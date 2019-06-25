9c60686d74376be696a4a38aec3f8b68495704b7
chffUA <chff@ua.pt>
2019-06-25 17:11:19 +0100

    Base project

---
+++ README.md
@@ -0,0 +1,49 @@
+# Console-colors
+
+Improve the JS console's capabilities without relying on more external dependencies. Add one of the two available files to a project and import the module from wherever necessary.
+
+Comes in two flavours:
+
+- **console-colors** (text colors, mix-and-match (text+background), many different colors in the same string.)
+
+- **console-colors-full** (text colors, background colors, mix-and-match (text+background), many different colors in the same string, define text+background colors (styles) for a single function call or set a default style.)
+
+Both modules inherit all functionality from the native ```console```, so you can safely replace it as such:
+
+```js 
+const console = require("./console-colors");
+```
+
+I recommend using **console-colors** if you don't want to read more.
+
+-----
+
+## Console-colors
+
+  - methods for coloring a line of text: ```bold(), black(), red(), green(), gold(), blue(), magenta(), cyan(), white(), grey(), gray(), ruby(), leaf(), yellow(), ocean(), pink(), sky(), light()```
+  
+  - method to create a string with the intended colors: ```format()```, which accepts a **style**. A **style** is an object that can have 2 keys:
+  
+      - **text**, which may be any of the colors mentioned above
+      
+      - **bg** or **background**, any of the colors above except **bold**
+  
+![](https://i.imgur.com/fineExl.png)
+
+-----
+
+## Console-colors-full
+
+  - everything console-colors does (see above)
+  
+  - set background mode: ```colorBackground(boolean)```, if ```true``` then methods such as ```red()``` will color the background instead of text
+  
+  - set a persistent style: ```setStyle() / clearStyle()```, if a style is set then all printing methods will try to respect it
+  
+       - methods such as ```red()``` that already define colors will ignore the style's text or background color (which one depends on background mode)
+       
+  - use a style for a specific print: ```slog()``` receives a style along with a message, and respects that style for only that call
+  
+![](https://i.imgur.com/wA9YnWN.png)
+
+![](https://i.imgur.com/Mk43ba8.png)
---
+++ console-colors.js
@@ -0,0 +1,121 @@
+module.exports = {
+    // main effects
+    bold: (message, ...optionalParams) => print(1,message,...optionalParams),
+    black: (message, ...optionalParams) => print(30,message,...optionalParams),
+    red: (message, ...optionalParams) => print(31,message,...optionalParams),
+    green: (message, ...optionalParams) => print(32,message,...optionalParams),
+    gold: (message, ...optionalParams) => print(33,message,...optionalParams),
+    blue: (message, ...optionalParams) => print(34,message,...optionalParams),
+    magenta: (message, ...optionalParams) => print(35,message,...optionalParams),
+    cyan: (message, ...optionalParams) => print(36,message,...optionalParams),
+    white: (message, ...optionalParams) => print(37,message,...optionalParams),
+    grey: (message, ...optionalParams) => print(90,message,...optionalParams),
+    gray: (message, ...optionalParams) => print(90,message,...optionalParams),
+    ruby: (message, ...optionalParams) => print(91,message,...optionalParams),
+    leaf: (message, ...optionalParams) => print(92,message,...optionalParams),
+    yellow: (message, ...optionalParams) => print(93,message,...optionalParams),
+    ocean: (message, ...optionalParams) => print(94,message,...optionalParams),
+    pink: (message, ...optionalParams) => print(95,message,...optionalParams),
+    sky: (message, ...optionalParams) => print(96,message,...optionalParams),
+    light: (message, ...optionalParams) => print(97,message,...optionalParams),
+    // other utils
+    log: (message, ...optionalParams) => log(message, ...optionalParams),
+    format: (aStyle, message) => format(aStyle, message),
+};
+
+// ------------- inherit --------------
+
+// these properties won't be shown/autocompleted by the IDE if added dynamically
+
+module.exports.debug = console.debug;
+module.exports.info = console.info;
+module.exports.warn = console.warn;
+module.exports.error = console.error;
+module.exports.dir = console.dir;
+module.exports.time = console.time;
+module.exports.timeEnd = console.timeEnd;
+module.exports.trace = console.trace;
+module.exports.assert = console.assert;
+module.exports.clear = console.clear;
+module.exports.count = console.count;
+module.exports.countReset = console.countReset;
+module.exports.group = console.group;
+module.exports.groupCollapsed = console.groupCollapsed;
+module.exports.groupEnd = console.groupEnd;
+module.exports.Console = console.Console;
+module.exports.dirxml = console.dirxml;
+module.exports.table = console.table;
+module.exports.markTimeline = console.markTimeline;
+module.exports.profile = console.profile;
+module.exports.profileEnd = console.profileEnd;
+module.exports.timeline = console.timeline;
+module.exports.timelineEnd = console.timelineEnd;
+module.exports.timeStamp = console.timeStamp;
+module.exports.context = console.context;
+
+// ------------ internals -------------
+
+const log = (text, ...params) => { // no predetermined colors
+    console.log(replaceResets(text, reset()), ...params, reset());
+}
+
+const print = (num, text, ...params) => { // predetermined color
+    console.log(effect(num)+replaceResets(text, effect(num)), ...params, reset());
+}
+
+const format = (st, text) => { // returns a block of text with the intended colors
+    let formatting = "";
+
+    if (st.text) {
+        let idx = getTextColorNumber(st.text);
+        if (idx!=-1) formatting += effect(idx);
+    }
+
+    if (!(st.bg && st.background)) { // having both is invalid, ignore
+        if (st.bg) {
+            let idx = getBackgroundNumber(st.bg);
+            if (idx!=-1) formatting += effect(idx);
+        }
+        if (st.background) {
+            let idx = getBackgroundNumber(st.background);
+            if (idx!=-1) formatting += effect(idx);
+        }
+    }
+
+    return formatting+prepareString(text)+reset();
+}
+
+const getBackgroundNumber = (name) => {
+    let bgStyles = ["black","red","green","gold","blue","magenta","cyan","white",
+                      "grey","gray","ruby","leaf","yellow","ocean","pink","sky","light"];
+
+    let indexes = [30,31,32,33,34,35,36,37,90,90,91,92,93,94,95,96,97];
+
+    let idx = bgStyles.indexOf(name);
+    if (idx==-1) return idx; else return indexes[idx]+10;
+}
+
+const getTextColorNumber = (name) => {
+    let textStyles = ["black","red","green","gold","blue","magenta","cyan","white",
+                      "grey","gray","ruby","leaf","yellow","ocean","pink","sky","light","bold"];
+
+    let indexes = [30,31,32,33,34,35,36,37,90,90,91,92,93,94,95,96,97,1];
+
+    let idx = textStyles.indexOf(name);
+    if (idx==-1) return idx; else return indexes[idx];
+}
+
+const effect = (num) => "["+num+"m"; //  = \x1b
+
+const reset = () => effect(0);
+
+const prepareString = (str) => {
+    if (typeof str === "object") return JSON.stringify(str);
+    else if (str === null) return "null";
+    else if (typeof str === "undefined") return "undefined";
+    else return str.toString();
+}
+
+const replaceResets = (str, f) => {
+    return prepareString(str).replace(/\[0m/g, reset()+f); // hacky way to fix log() with format()
+}
